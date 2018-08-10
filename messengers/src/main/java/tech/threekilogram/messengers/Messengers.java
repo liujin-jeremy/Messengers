cba1e524105e37869807261fc3780637c9da34df
liujin <wuxiongc@163.com>
2018-08-10 21:41:00 +0800

    更新注释

--- messengers/src/main/java/tech/threekilogram/messengers/Messengers.java
+++ messengers/src/main/java/tech/threekilogram/messengers/Messengers.java
@@ -10,16 +10,39 @@ import java.lang.ref.WeakReference;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * @author wuxio 2018-05-01:20:16 该类用于两个类之间通信 用于发送一个通知,主要用于后台任务处理完成之后,继续下一步任务, 使用handler
- *     1.解决方法栈过长,2.可以切换到主线程
+ * 该类用于两个类之间通信 用于发送一个通知
+ * 主要用于后台任务处理完成之后,继续下一步任务,
+ * 使用handler:
+ * 1.解决方法栈过长,
+ * 2.可以切换到主线程
+ *
+ * @author wuxio 2018-05-01:20:16
  */
 public class Messengers {
 
-      private static final AtomicInteger ATOMIC_INTEGER = new AtomicInteger();
+      /**
+       * 用来生成index
+       */
+      private static final AtomicInteger MESSAGE_INDEX = new AtomicInteger();
+      /**
+       * 使用一个{@link HandlerThread}单独处理消息
+       */
       private static ReceiveHandler sSendHandler;
+      /**
+       * 使用主线程处理消息
+       */
       private static ReceiveHandler sMainHandler;
 
-      public static void init ( ) {
+      static {
+
+            /* 初始化自己 */
+            init();
+      }
+
+      /**
+       * 初始化变量
+       */
+      private static void init ( ) {
 
             /* 防止多次初始化 */
 
@@ -29,18 +52,8 @@ public class Messengers {
 
             HandlerThread thread = new HandlerThread( "Messengers" );
             thread.start();
+
             sSendHandler = new ReceiveHandler( thread.getLooper() );
-            sMainHandler = new ReceiveHandler( Looper.getMainLooper() );
-      }
-
-      /**
-       * 使用一个非主线程looper初始化
-       *
-       * @param looper 非主线程looper
-       */
-      public static void init ( Looper looper ) {
-
-            sSendHandler = new ReceiveHandler( looper );
             sMainHandler = new ReceiveHandler( Looper.getMainLooper() );
       }
 
@@ -65,6 +78,17 @@ public class Messengers {
       }
 
       /**
+       * 发送一条空白消息,携带一个数据
+       *
+       * @param what 标识,如果是奇数,发送到主线程,如果时偶数,发送到后台线程处理,注意不要使用0作为标识
+       * @param who 发送给谁
+       */
+      public static void send ( int what, Object extra, @NonNull OnMessageReceiveListener who ) {
+
+            send( what, 0, extra, who );
+      }
+
+      /**
        * 发送一条消息,携带一个数据
        *
        * @param what 标识,如果是奇数,发送到主线程,如果时偶数,发送到后台线程处理,注意不要使用0作为标识
@@ -73,10 +97,16 @@ public class Messengers {
        * @param who 发送给谁
        */
       public static void send (
-          int what, int delayed, Object extra, @NonNull OnMessageReceiveListener who ) {
+          int what,
+          int delayed,
+          Object extra,
+          @NonNull OnMessageReceiveListener who ) {
 
             final int judge = 2;
             ReceiveHandler sendHandler;
+
+            /* 判断消息发送到哪个线程 */
+
             if( what % judge == 0 ) {
                   sendHandler = sSendHandler;
             } else {
@@ -84,9 +114,12 @@ public class Messengers {
             }
 
             Message obtain = Message.obtain();
-            int key = ATOMIC_INTEGER.addAndGet( 1 );
-            obtain.arg1 = key;
+            int key = MESSAGE_INDEX.addAndGet( 1 );
             sendHandler.MESSAGE_HOLDER_ARRAY.put( key, new Holder( what, extra, who ) );
+
+            /* handler 使用该标识获取holder */
+            obtain.arg1 = key;
+            /* 用户设置的消息标识 */
             obtain.what = what;
 
             sendHandler.sendMessageDelayed( obtain, delayed );
@@ -103,23 +136,13 @@ public class Messengers {
             send( what, delayed, null, who );
       }
 
-      /**
-       * 发送一条空白消息,携带一个数据
-       *
-       * @param what 标识,如果是奇数,发送到主线程,如果时偶数,发送到后台线程处理,注意不要使用0作为标识
-       * @param who 发送给谁
-       */
-      public static void send ( int what, Object extra, @NonNull OnMessageReceiveListener who ) {
-
-            send( what, 0, extra, who );
-      }
-
       //============================ 移除一条消息监听 ============================
 
       /**
        * 移除一条消息
        *
        * @param what message 标识
+       * @param listener 该消息发送给谁
        */
       public static void remove ( int what, OnMessageReceiveListener listener ) {
 
@@ -150,6 +173,8 @@ public class Messengers {
 
                   if( holder != null && holder.what == what && holder.listener.get() == listener ) {
                         holder.listener.clear();
+
+                        /* 不移除原因:因为线程不安全,防止正在处理消息时空指针异常,在处理消息时,会自动处理该holder */
                   }
             }
       }
@@ -157,10 +182,14 @@ public class Messengers {
       //============================ send messenger async ============================
 
       /**
-       * 发送消息的handler
+       * 接收{@link #send(int, OnMessageReceiveListener)}发送的消息,然后转发给监听消息{@link
+       * OnMessageReceiveListener}
        */
       private static class ReceiveHandler extends Handler {
 
+            /**
+             * 保存需要转发的消息
+             */
             final SparseArray<Holder> MESSAGE_HOLDER_ARRAY = new SparseArray<>();
 
             /**
@@ -209,7 +238,13 @@ public class Messengers {
        */
       private static class Holder {
 
+            /**
+             * 消息标识
+             */
             private int    what;
+            /**
+             * 消息携带的额外信息
+             */
             private Object extra;
 
             /**
